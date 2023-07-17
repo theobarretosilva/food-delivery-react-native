7f497c081753d86b17ea6c9b568c92d2df69d8e9
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-17 00:47:14 -0300

    fazendo CartScreen

--- screens/CartScreen/CartScreen.js
+++ screens/CartScreen/CartScreen.js
@@ -2,13 +2,15 @@ import { TouchableOpacity, View } from "react-native";
 import { styles } from "./CartScreen.styles";
 import { featured } from '../../constants/index'
 import * as Icon from 'react-native-feather';
+import { useNavigation } from '@react-navigation/native'
 
 export default function CartScreen() {
     const restaurant = featured.restaurants[0];
+    const navigation = useNavigation();
     return(
         <View style={styles.geralView}>
             <View style={styles.viewBackBtn}>
-                <TouchableOpacity style={styles.touchable} onPress={}>
+                <TouchableOpacity style={styles.touchable} onPress={() => navigation.goBack()}>
                     <Icon.ArrowLeft strokeWidth={3} stroke={'white'}  />
                 </TouchableOpacity>
             </View>
